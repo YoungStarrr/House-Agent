a10147b9181bd47d12b3fda34ff9a2ed14225a27
* <okikiolab769@gmail.com>
2025-05-17 23:40:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function buttonElement() {
+  alert('This is sample website page from web creator');
+  console.log("This is sample website page from web creator");
+}
---
+++ services.js
@@ -0,0 +1,4 @@
+function rent() {
+  alert('This is sample website page from web creator');
+  console.log("This is sample website page from web creator");
+}
